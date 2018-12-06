aa2ab16191f401f76010e382e4ad0dc688fb9b3c
delightful-range-7379 <khaled.khalifeh@gmail.com>
2018-12-06 19:15:14 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,22 +1,18 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push("I am 1 strange loop.");
-    }
-    else {
-      array.push(`I am ${i} strange loops.`);
-    }
-  }
-  return array;
+ for (i=0;i<25;i++) {
+   if (i===1) 
+   array.push("I am 1 strange loop.") ;
+   else
+   array.push(`I am ${i} strange loops.`)
+ }
+ return array;
 }
 
-function whileLoop(number) {
-  let countdown = number;
-  
-  while (countdown>0) {
-    console.log(--countdown);
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
   }
-  return "done";
+  return 'done';
 }
 
 function incrementVariable() {
@@ -24,11 +20,11 @@ function incrementVariable() {
 }
 
 function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
+  do {
+    array.shift(); } 
+    while (array.length > 0 && incrementVariable())
+}
+
   }
-   do {
-    array.pop();
-  } while (array.length > -1 && maybeTrue());
-    return array;
+  
 }
